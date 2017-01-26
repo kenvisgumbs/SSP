b0e54a9944b651e8c3a2933265ec9c18b327815b
kenvisgumbs <kgumbs@codeary.com>
2017-01-26 16:11:12 -0400

    Second Commit

---
+++ SSBP/SSBP/TEST LOGIN.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TEST LOGIN.aspx.cs" Inherits="SSBP.TEST_LOGIN" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="MenuName" runat="server" Text="User Registration Test"></asp:Label>
+        <br />  
+        <asp:Literal ID="StatusMessage" runat="server"></asp:Literal>
+        <br />
+        <asp:Label ID="UserName" runat="server" Text="User Name"></asp:Label>
+        <br />
+        <asp:TextBox ID="UserNameInput" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Password" runat="server" Text="Password"></asp:Label>
+        <br />
+        <asp:TextBox ID="PasswordInput" runat="server" TextMode="Password"></asp:TextBox>
+        <br />  
+        <asp:Button ID="RegisterButton" runat="server" Text="Register" OnClick="RegisterButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ SSBP/SSBP/TEST LOGIN.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SSBP
+{
+    public partial class TEST_LOGIN : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RegisterButton_Click(object sender, EventArgs e)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var user = new IdentityUser(){ UserName = UserNameInput.Text};
+
+            IdentityResult result = manager.Create(user, PasswordInput.Text);
+
+            if (result.Succeeded)
+            {
+                StatusMessage.Text = String.Format("User {0} has been successfully registered", user.UserName);
+            }
+            else
+            {
+                StatusMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
